478f7357228e7530862a59b3bd8d1eb9d131ffc9
Vaclav Rozsypalek <vaclav@giantswarm.io>
2017-11-07 14:14:45 +0100

    fix tests

--- service/flannel_test.go
+++ service/flannel_test.go
@@ -9,20 +9,20 @@ import (
 
 func Test_Flannel_ParseIP(t *testing.T) {
 	tests := []struct {
-		config             func(flannelFile []byte) (network.Network, error)
+		config             func(flannelFile []byte) (string, error)
 		flannelFileContent []byte
-		expectedConfig     network.Network
+		expectedIP         string
 		expectedErr        error
 	}{
 		// test 0
 		{
-			config: func(flannelFile []byte) (network.Network, error) {
+			config: func(flannelFile []byte) (string, error) {
 				conf := DefaultConfig()
 				conf.Flag = flag.New()
 				err := conf.parseIPs(flannelFile)
-				return conf.Flag.Service.NetworkConfig, err
+				return conf.Flag.Service.IPAddress, err
 			},
-			expectedConfig: network.Network{FlannelIP: "172.23.3.64", BridgeIP: "172.23.3.65"},
+			expectedIP: "172.23.3.66",
 			flannelFileContent: []byte(`FLANNEL_NETWORK=172.23.3.0/24
 FLANNEL_SUBNET=172.23.3.65/30
 FLANNEL_MTU=1450
@@ -31,13 +31,13 @@ FLANNEL_IPMASQ=false`),
 		},
 		// test 1
 		{
-			config: func(flannelFile []byte) (network.Network, error) {
+			config: func(flannelFile []byte) (string, error) {
 				conf := DefaultConfig()
 				conf.Flag = flag.New()
 				err := conf.parseIPs(flannelFile)
-				return conf.Flag.Service.NetworkConfig, err
+				return conf.Flag.Service.IPAddress, err
 			},
-			expectedConfig: network.Network{FlannelIP: "198.168.0.0", BridgeIP: "198.168.0.1"},
+			expectedIP: "198.168.0.2",
 			flannelFileContent: []byte(`FLANNEL_NETWORK=198.168.0.0/24
 FLANNEL_SUBNET=198.168.0.1/30
 FLANNEL_MTU=1450
@@ -46,13 +46,13 @@ FLANNEL_IPMASQ=false`),
 		},
 		// test 2 - missing FLANNEL_SUBNET
 		{
-			config: func(flannelFile []byte) (network.Network, error) {
+			config: func(flannelFile []byte) (string, error) {
 				conf := DefaultConfig()
 				conf.Flag = flag.New()
 				err := conf.parseIPs(flannelFile)
-				return conf.Flag.Service.NetworkConfig, err
+				return conf.Flag.Service.IPAddress, err
 			},
-			expectedConfig: network.Network{},
+			expectedIP: "",
 			flannelFileContent: []byte(`FLANNEL_NETWORK=192.168.0.0/24
 FLANNEL_MTU=1450
 FLANNEL_IPMASQ=false`),
@@ -60,13 +60,13 @@ FLANNEL_IPMASQ=false`),
 		},
 		// test 3 - invalid subnet in kvm file
 		{
-			config: func(flannelFile []byte) (network.Network, error) {
+			config: func(flannelFile []byte) (string, error) {
 				conf := DefaultConfig()
 				conf.Flag = flag.New()
 				err := conf.parseIPs(flannelFile)
-				return conf.Flag.Service.NetworkConfig, err
+				return conf.Flag.Service.IPAddress, err
 			},
-			expectedConfig: network.Network{},
+			expectedIP: "",
 			flannelFileContent: []byte(`FLANNEL_NETWORK=198.168.0.0/24
 FLANNEL_SUBNET=_x.68.c.0/30
 FLANNEL_MTU=1450
@@ -75,25 +75,25 @@ FLANNEL_IPMASQ=false`),
 		},
 		// test 4 - empty kvm file
 		{
-			config: func(flannelFile []byte) (network.Network, error) {
+			config: func(flannelFile []byte) (string, error) {
 				conf := DefaultConfig()
 				conf.Flag = flag.New()
 				err := conf.parseIPs(flannelFile)
-				return conf.Flag.Service.NetworkConfig, err
+				return conf.Flag.Service.IPAddress, err
 			},
-			expectedConfig:     network.Network{},
+			expectedIP:         "",
 			flannelFileContent: []byte(``),
 			expectedErr:        invalidFlannelConfigurationError,
 		},
 		// test 5 - non kvm file
 		{
-			config: func(flannelFile []byte) (network.Network, error) {
+			config: func(flannelFile []byte) (string, error) {
 				conf := DefaultConfig()
 				conf.Flag = flag.New()
 				err := conf.parseIPs(flannelFile)
-				return conf.Flag.Service.NetworkConfig, err
+				return conf.Flag.Service.IPAddress, err
 			},
-			expectedConfig: network.Network{},
+			expectedIP: "",
 			flannelFileContent: []byte(`machine:
   services:
     - docker
@@ -110,17 +110,14 @@ dependencies:
 	}
 
 	for index, test := range tests {
-		networkConfig, err := test.config(test.flannelFileContent)
+		ip, err := test.config(test.flannelFileContent)
 
 		if microerror.Cause(err) != microerror.Cause(test.expectedErr) {
 			t.Fatalf("%d: unexcepted error, expected %#v but got %#v", index, test.expectedErr, err)
 		}
 		if test.expectedErr == nil {
-			if strings.Compare(networkConfig.FlannelIP, test.expectedConfig.FlannelIP) != 0 {
-				t.Fatalf("%d: Incorrent ip, expected %s but got %s.", index, test.expectedConfig.FlannelIP, networkConfig.FlannelIP)
-			}
-			if strings.Compare(networkConfig.BridgeIP, test.expectedConfig.BridgeIP) != 0 {
-				t.Fatalf("%d: Incorrent ip, expected %s but got %s.", index, test.expectedConfig.BridgeIP, networkConfig.BridgeIP)
+			if strings.Compare(ip, test.expectedIP) != 0 {
+				t.Fatalf("%d: Incorrent ip, expected %s but got %s.", index, test.expectedIP, ip)
 			}
 		}
 	}
